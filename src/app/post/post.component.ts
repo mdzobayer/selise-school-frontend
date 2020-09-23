61d7f060f0772a466d443c8fde2d04fc124a1e2e
mdzobayer <mdzobayer@rocketmail.com>
2020-09-23 12:32:32 +0600

    background image added

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  blogImg = "https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+
   constructor() { }
 
   ngOnInit(): void {
